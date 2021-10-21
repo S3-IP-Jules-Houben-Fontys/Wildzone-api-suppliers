3f23822a60841556fac862903d259e3526726e63
Jules <jules.houben@live.nl>
2021-10-21 14:40:53 +0200

    Fixed env location for config, changed folders

--- app/config/config.js
+++ app/config/config.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
+require('dotenv').config({path: '../.env'});
 module.exports = {
-    "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "dialect": process.env.DB_DIALECT,
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
     },
     "test": {
         "username": "root",
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/*
+const db = require('./app/models');
+db.sequelize.sync().then(() =>{
+    console.log("Drop and re-sync db.");
+});
+*/
+
+app.get('/', (req, res) =>{
+    res.json({message: 'Hello world!'});
+});
+
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () =>{
+    console.log(`listening on port ${PORT}`);
+});
